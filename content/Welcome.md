3bb815e76fe2a38e5eb9c223dff0b073e423cd6b
洪沐天 <1747638932@qq.com>
2024-08-23 22:31:56 -0700

    Quartz sync: Aug 23, 2024, 10:31 PM

---
+++ content/Welcome.md
@@ -0,0 +1,8 @@
+---
+title: Welcome
+publish: false
+tags:
+---
+# Welcome to the Quartz of Ori (Mutian Hong)!
+
+
--- quartz/plugins/filters/draft.ts
+++ quartz/plugins/filters/draft.ts
@@ -3,7 +3,9 @@ import { QuartzFilterPlugin } from "../types"
 export const RemoveDrafts: QuartzFilterPlugin<{}> = () => ({
   name: "RemoveDrafts",
   shouldPublish(_ctx, [_tree, vfile]) {
-    const draftFlag: boolean = vfile.data?.frontmatter?.draft || false
-    return !draftFlag
+    // const draftFlag: boolean = vfile.data?.frontmatter?.draft || false
+    // return !draftFlag
+    const publishFlag: boolean = vfile.data?.frontmatter?.publish || false
+    return publishFlag
   },
 })
